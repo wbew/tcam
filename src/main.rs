29a59a9f26883ff4628c86eefdb1c334169766f6
Will Wang <001wwang@gmail.com>
2026-01-05 21:14:46 -0500

    Open camera

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use nokhwa::{
+    Camera,
+    pixel_format::RgbFormat,
+    utils::{CameraIndex, RequestedFormat, RequestedFormatType},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let mut camera = Camera::new(
+        // Which camera to use
+        CameraIndex::default(),
+        // A format for the camera output (e.g. 1024x1024, MP4)
+        RequestedFormat::new::<RgbFormat>(RequestedFormatType::AbsoluteHighestFrameRate),
+    );
 }
